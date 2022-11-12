cc493341c639920b01d88e3dc5be46b5d6900893
jennycoranguez <lunajennykiara@gmail.com>
2022-11-11 22:46:21 -0700

    change the route for home page

--- src/App.js
+++ src/App.js
@@ -7,21 +7,25 @@ import Portfolio from './components/Portfolio'
 import Resume from "./components/Resume"
 
 
-
 function App() {
 
   return (
     <>
+  
+    
       <Header></Header>
+      
       <Routes>
-        <Route exact path="/" element={<About />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      
+          <Route  exact path="/"  element={<About/>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path='/React_Portfolio' element={<About />} />
+       </Routes>
     </>
-
+    
   );
 }
 
